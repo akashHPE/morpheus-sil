61d4bdeca9e8efd83d8c9ba21beb693b22d4dfac
akashHPE <155995494+akashHPE@users.noreply.github.com>
2024-04-10 16:53:39 -0400

    Update sid-minibert-20230424-script.py
    Replaced torch.save in the checkpoint block with the export_onnx call.

--- sid-minibert-20230424-script.py
+++ sid-minibert-20230424-script.py
@@ -179,10 +179,11 @@ def train_model(model_dir, train_dataloader, idx2label, core_context, sample_mod
         if (idx+1) % int(args.checkpoint_every_n_epochs) == 0:
             print("checkpointing now")
             with core_context.checkpoint.store_path(checkpoint_metadata_dict) as (path, storage_id):
-                torch.save(model.state_dict(), path / "checkpoint.pt")
+                export_onnx(model, path, sample_model_input)
+                #torch.save(model.state_dict(), path / "checkpoint.pt")
                 with path.joinpath("state").open("w") as f:
                     f.write(f"{idx+1},{info.trial.trial_id}")
-                export_onnx(model, path, sample_model_input)
+                #export_onnx(model, path, sample_model_input)
 
             if core_context.preempt.should_preempt():
                 return
