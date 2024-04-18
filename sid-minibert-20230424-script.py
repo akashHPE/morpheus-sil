1717e8b921eae3af82c10d70eb81e82921fc0432
akashHPE <155995494+akashHPE@users.noreply.github.com>
2024-04-18 13:02:43 -0400

    Update sid-minibert-20230424-script.py
    removed faulty tokenizer.cuda()

--- sid-minibert-20230424-script.py
+++ sid-minibert-20230424-script.py
@@ -102,7 +102,6 @@ def train_model(model_dir, train_dataloader, idx2label, core_context, sample_mod
     tokenizer = cased_tokenizer
     model.train()
     model.cuda()
-    tokenizer.cuda()
     # use DataParallel if you have more than one GPU
     if torch.cuda.device_count() > 1:
         model = torch.nn.DataParallel(model)
