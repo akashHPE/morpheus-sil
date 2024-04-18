4ac55c3a236b6db9c4259f60144d667b4bc90255
akashHPE <155995494+akashHPE@users.noreply.github.com>
2024-04-18 15:28:05 -0400

    Update sid-minibert-20230424-script.py
    updated tokenizer checkpointing to tokenizer.save(path / tokenizer.json)

--- sid-minibert-20230424-script.py
+++ sid-minibert-20230424-script.py
@@ -181,7 +181,8 @@ def train_model(model_dir, train_dataloader, idx2label, core_context, sample_mod
             print("checkpointing now")
             with core_context.checkpoint.store_path(checkpoint_metadata_dict) as (path, storage_id):
                 model.save_pretrained(path)
-                tokenizer.save_pretrained(path)
+                tokenizer.save(path / tokenizer.json')
+                #tokenizer.save_pretrained(path)
                 torch.save(sample_model_input, path / "sample_model_input.pt")
                 with path.joinpath("state").open("w") as f:
                     f.write(f"{idx+1},{info.trial.trial_id}")
